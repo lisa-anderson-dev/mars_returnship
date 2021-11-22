842d6a48fef4cf568e1a75e046261aae4c168c0b
lisa <lisachananderson@gmail.com>
2021-11-22 15:33:15 -0800

    new commit

---
+++ 20211122_contactList/root/scripts/script.js
@@ -0,0 +1,74 @@
+class Contact {
+    #fullName;
+    #email;
+    #city;
+    #country;
+
+    get fullName() {
+        return this.#fullName;
+    }
+
+    set fullName(fullName) {
+        this.#fullName = fullName;
+    }
+
+    get email() {
+        return this.#email;
+    }
+
+    set email(email) {
+        this.#email = email;
+    }
+
+    get city() {
+        return this.#city;
+    }
+
+    set city(city) {
+        if (!city)
+            this.#city = "";
+        else
+            this.#city = city;
+    }
+
+    get country() {
+        return this.#country;
+    }
+
+    set country(country) {
+        if (!country)
+            this.#country = "";
+        else
+            this.#country = country;
+    }
+}
+
+let contactProps = ["fullName", "email", "city", "country"];
+
+function addContact() {
+    let fullName = document.getElementById("fullName").value;
+    let email = document.getElementById("email").value;
+    let city = document.getElementById("city").value;
+    let country = document.getElementById("country").value;
+    if (!fullName || !email) 
+        alert("Full Name and Email are required fields.");
+    else {
+        let newContact = new Contact();
+        newContact.fullName = fullName;
+        newContact.email = email;
+        newContact.city = city;
+        newContact.country = country;
+        buildContactList(newContact);
+    }   
+}
+
+function buildContactList(newContact) {
+    let contactTable = document.getElementById("contactList");
+    let numRows = document.querySelectorAll("#contactList tr").length;
+    let row = contactTable.insertRow(numRows);
+    for (let i = 0; i < contactProps.length; i++) {
+        let cell = row.insertCell(i);
+        cell.innerHTML = newContact[contactProps[i]];
+    }
+    document.getElementById("addContactForm").reset();
+}
